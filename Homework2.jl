5900e0e490821531055207d78cc0360a0205ba5d
Jonathan Gouws <jgouws@perimeterinstitute.ca>
2024-02-11 13:24:12 -0500

    Did somemore work on the homework

--- Homework2.jl
+++ Homework2.jl
@@ -1,17 +1,21 @@
 using Pkg
 
-Pkg.add("WGLMakie")
+Pkg.add("CairoMakie")
 Pkg.add("HCubature")
 Pkg.add("SphericalHarmonics")
 Pkg.add("LegendrePolynomials")
 Pkg.add("DifferentialEquations")
+Pkg.add("LaTeXStrings")
 
 
-using WGLMakie
+using Printf
+using LaTeXStrings
+using CairoMakie
 using HCubature
 using SphericalHarmonics
 using LegendrePolynomials
 using DifferentialEquations
+
 
 function flattenSHArray(shArray, lₘₐₓ)
     l = 0
@@ -29,6 +33,24 @@ function flattenSHArray(shArray, lₘₐₓ)
     coeffVector
 end
 
+function flattenSHArray(shArray)
+    l = 0
+    m = 0
+    coeffVector::Array{ComplexF64} = []
+    for a in 1:size(shArray)[1]
+        push!(coeffVector, shArray[(l, m)])
+        if m == l
+            l+=1
+            m = -l
+        else
+            m+=1
+        end
+    end
+    coeffVector
+end
+
+#Funciton to expand a function in spherical harmonics
+#This just integrates to work out coefficients
 expandSphericalHarmonics2(f, lₘₐₓ) = hcubature(x -> sin(x[1]) * f(x[1], x[2]) .* conj(flattenSHArray(computeYlm(x[1], x[2], lmax = lₘₐₓ))), [0, 0], [π, 2 * π])
 
 function LaplacianSphericalCoefficients(coeffs, lₘₐₓ)
@@ -40,6 +62,8 @@ function LaplacianSphericalCoefficients(coeffs, lₘₐₓ)
     coeffs
 end
 
+#Calculate the Laplacian of a representation of a function
+#in spherical harmonics given the coefficients
 function LaplacianSphericalCoefficientsArays(coeffs, lₘₐₓ)
     l = 0
     m = 0
@@ -72,30 +96,6 @@ function LaplacianSphericalCoefficientsArrays(coeffs)
     coeffVector
 end
 
-function flattenSHArray(shArray)
-    l = 0
-    m = 0
-    coeffVector::Array{ComplexF64} = []
-    for a in 1:size(shArray)[1]
-        push!(coeffVector, shArray[(l, m)])
-        if m == l
-            l+=1
-            m = -l
-        else
-            m+=1
-        end
-    end
-    coeffVector
-end
-
-chunk(arr, n) = [arr[i:min(i + n - 1, end)] for i in 1:n:length(arr)]
-
-function waveSystem(U)
-    half = Int(size(U)[1] // 2)
-    ϕ = U[begin:half]
-    ψ = U[half + 1:end]    
-    vcat(ψ, LaplacianSphericalCoefficientsArrays(ϕ))
-end
 
 ##ϕ₀(θ, ϕ) = exp(- 4 * θ^4);
 ##ψ₀(θ, ϕ) = 0;
@@ -107,6 +107,7 @@ end
 ##prob = ODEProblem((U,p,t) -> waveSystem(U), U0, (0.0, 10.0))
 ##sol = solve(prob);
 
+#Calculate a function from spherical harmonic represntation coefficients
 function functionFromSphericalCoefficients(coeffs)
     l = 0
     m = 0
@@ -126,14 +127,21 @@ function functionFromSphericalCoefficients(coeffs)
     end
 end
 
+function waveSystem(U)
+    half = Int(size(U)[1] // 2)
+    ϕ = U[begin:half]
+    ψ = U[half + 1:end]    
+    vcat(ψ, LaplacianSphericalCoefficientsArrays(ϕ))
+end
+
 function solveWaveEquation(ϕ₀, ψ₀, N, t₀, t₁)
   ϕ₀SphericalCoeffs = expandSphericalHarmonics2(ϕ₀, N)[1];
   ψ₀SphericalCoeffs = expandSphericalHarmonics2(ψ₀, N)[1];
 
-
-  U0 = vcat( ϕ₀SphericalCoeffs, N, ψ₀SphericalCoeffs, N)
+  alg = DP5()
+  U0 = vcat( ϕ₀SphericalCoeffs, ψ₀SphericalCoeffs)
   prob = ODEProblem((U,p,t) -> waveSystem(U), U0, (t₀, t₁))
-  solve(prob);
+  solve(prob, alg)
 end
 
 ###
@@ -388,69 +396,159 @@ end
 #
 #exit()
 
-ϕ₀(θ, ϕ) = cos(8 * θ);
-ψ₀(θ, ϕ) = 0;
 
-sol = solveWaveEquation(ϕ₀, ψ₀, 10, 0, 10)
+for lmax in [2, 4, 6, 8, 10]
+  ϕ₀(θ, ϕ) = exp( - θ^2 / 0.4);
+  ψ₀(θ, ϕ) = 0;
+  sol = solveWaveEquation(ϕ₀, ψ₀, lmax, 0, 10)
 
-sizA = Int(size(sol[1])[1] / 2)
+  #Only take the first items
+  sizA = Int(size(sol.u[1])[1] / 2)
 
-θs = LinRange(0, π, 100)
-ϕs = LinRange(0,  2 * π, 100)
+  θs = LinRange(0, π, 100)
+  ϕs = LinRange(0,  2 * π, 100)
 
-xs = [sin(θ) * cos(ϕ) for θ in θs, ϕ in ϕs]
+  xs = [sin(θ) * cos(ϕ) for θ in θs, ϕ in ϕs]
 
-ys = [sin(θ) * sin(ϕ) for θ in θs, ϕ in ϕs]
+  ys = [sin(θ) * sin(ϕ) for θ in θs, ϕ in ϕs]
 
-zs = [cos(θ) for θ in θs, ϕ in ϕs]
+  zs = [cos(θ) for θ in θs, ϕ in ϕs]
 
-firstprofileWhole = sol[1][begin:sizA]
-myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)
+  firstprofileWhole = sol[1][begin:sizA]
+  myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)
 
-cols = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+  cols = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
 
-points = Observable(zs)
-HB_vn = Observable(cols)
+  points = Observable(zs)
+  HB_vn = Observable(cols)
 
-fps = 1
-nframes = size(sol.t)[1]
+  fps = 10
+  nframes = size(sol.t)[1]
 
-scene = surface(xs, ys, points, color = HB_vn, axis=(type=Axis3,), colorrange=(-0.5,0.5), colormap = reverse(cgrad(:redsblues)))
+  scene = surface(xs, ys, points, color = HB_vn, axis=(type=Axis3,), colorrange=(-0.5,1), colormap = reverse(cgrad(:thermal)))
 
-record(scene, "cosWaveSurfaceOnSphereFromScript.gif"; framerate = fps) do io
-    for it = 1:nframes
-        firstprofileWhole = sol[it][begin:sizA]
-        myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)    
-        cols = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
-        points[] = zs
-        HB_vn[] = cols
-        recordframe!(io)
-    end
+  record(scene, "GaussianWaveSurfaceOnSphereFromScript"*string(lmax)*".gif"; framerate = fps) do io
+      for it = 1:nframes
+          firstprofileWhole = sol[it][begin:sizA]
+          myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)    
+          cols = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+          points[] = zs
+          HB_vn[] = cols
+          recordframe!(io)
+      end
+  end
+
+  firstprofileWhole = sol[1][begin:sizA]
+  myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)
+
+  zs = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+
+  HB_vn = points = Observable(zs)
+
+  fps = 10
+  nframes = size(sol)[2]
+
+  scene = surface(θs, ϕs, HB_vn, axis=(type=Axis3,), colorrange=(-1,1), colormap = reverse(cgrad(:thermal)))
+
+
+  record(scene, "GaussianWaveSurfaceFromScript"*string(lmax)*".gif"; framerate = fps) do io
+      for it = 1:nframes
+          firstprofileWhole = sol[it][begin:sizA]
+          myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)    
+          zs = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+      
+          HB_vn[] = zs
+          recordframe!(io)
+      end
+  end
+
+  nslices = 5
+
+  f = Figure(size=(520, nslices * 320))#Figure(size=(400, trunc(Int, 400 * 5)))
+  for (k, i) in enumerate(1:trunc(Int, nframes / nslices):nframes)#trunc(Int, nframes / 5 + 1):nframes
+    Axis3(f[k, 1], viewmode = :fit)
+    firstprofileWhole = sol[i][begin:sizA]
+    myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)    
+    zs = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+    surface!(θs, ϕs, zs, colorrange=(-1,1), colormap = reverse(cgrad(:thermal)))
+  end
+
+  save("GaussianWaveSurfaceFromScriptTimes"*string(lmax)*".png", f)
+
+  f = Figure(size=(520, nslices * 320))#Figure(size=(400, trunc(Int, 400 * 5)))
+  for (k, i) in enumerate(1:trunc(Int, nframes / nslices):nframes)#trunc(Int, nframes / 5 + 1):nframes
+    solTime = @sprintf "%.2f" sol.t[i]
+#    title=L"Solution at time $t = %$(solTime)$"
+    ax = Axis(f[k, 1], title=L"Solution at time $t = %$(solTime)$", xlabel = L"\theta", ylabel = L"\phi")#@sprintf "Solution at t = %.2fs" sol.t[i], xlabel = L"\sum_a^b{xy} + \mathscr{L}", ylabel = L"\sum_a^b{xy} + \mathscr{L}")
+    firstprofileWhole = sol[i][begin:sizA]
+    myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)    
+    zs = (y -> real(myFuncFirst(y, pi / 2))).(θs)
+    lines!(θs, zs)
+  end
+  save("GaussianWaveSurfaceFromScriptTimesSlice"*string(lmax)*".png", f)
 end
 
-θs = LinRange(0, π, 100)
-ϕs = LinRange(0, 2 * π, 100)
+#ϕ₀(θ, ϕ) = cos(θ)^2;
+#ψ₀(θ, ϕ) = 0;
+#
+#sol = solveWaveEquation(ϕ₀, ψ₀, 7, 0, 20)
+#
+#sizA = Int(size(sol.u[1])[1] / 2)
+#
+#θs = LinRange(0, π, 100)
+#ϕs = LinRange(0,  2 * π, 100)
+#
+#xs = [sin(θ) * cos(ϕ) for θ in θs, ϕ in ϕs]
+#
+#ys = [sin(θ) * sin(ϕ) for θ in θs, ϕ in ϕs]
+#
+#zs = [cos(θ) for θ in θs, ϕ in ϕs]
+#
+#firstprofileWhole = sol[1][begin:sizA]
+#myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)
+#
+#cols = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+#
+#points = Observable(zs)
+#HB_vn = Observable(cols)
+#
+#fps = 10
+#nframes = size(sol.t)[1]
+#
+#scene = surface(xs, ys, points, color = HB_vn, axis=(type=Axis3,), colorrange=(-0,1), colormap = reverse(cgrad(:thermal)))
+#
+#record(scene, "cosSquaredWaveSurfaceOnSphereFromScript.gif"; framerate = fps) do io
+#    for it = 1:nframes
+#        firstprofileWhole = sol[it][begin:sizA]
+#        myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)    
+#        cols = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+#        points[] = zs
+#        HB_vn[] = cols
+#        recordframe!(io)
+#    end
+#end
+#
+#firstprofileWhole = sol[1][begin:sizA]
+#myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)
+#
+#zs = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+#
+#HB_vn = points = Observable(zs)
+#
+#fps = 10
+#nframes = size(sol)[2]
+#
+#scene = surface(θs, ϕs, HB_vn, axis=(type=Axis3,), colorrange=(0,1), colormap = reverse(cgrad(:thermal)))
+#
+#
+#record(scene, "cosSquaredWaveSurfaceFromScript.gif"; framerate = fps) do io
+#    for it = 1:nframes
+#        firstprofileWhole = sol[it][begin:sizA]
+#        myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)    
+#        zs = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
+#    
+#        HB_vn[] = zs
+#        recordframe!(io)
+#    end
+#end
 
-firstprofileWhole = sol[1][begin:sizA]
-myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)
-
-zs = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
-
-HB_vn = points = Observable(zs)
-
-fps = 1
-nframes = size(sol)[2]
-
-scene = surface(θs, ϕs, HB_vn, axis=(type=Axis3,), colorrange=(-0.5,0.5), colormap = reverse(cgrad(:redsblues)))
-
-
-record(scene, "cosWaveSurfaceFromScript.gif"; framerate = fps) do io
-    for it = 1:nframes
-        firstprofileWhole = sol[it][begin:sizA]
-        myFuncFirst = functionFromSphericalCoefficients(firstprofileWhole)    
-        zs = [real(myFuncFirst(θ, ϕ)) for θ in θs, ϕ in ϕs]
-    
-        HB_vn[] = zs
-        recordframe!(io)
-    end
-end
